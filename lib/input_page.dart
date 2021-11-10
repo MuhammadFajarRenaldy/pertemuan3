a03c51fccf8f00b04e16cb6365e8641470a30c65
MuhammadFajarRenaldy <you@example.com>
2021-11-10 20:57:19 +0800

    commit kedua

---
+++ lib/input_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const inActiveCardColor = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inActiveCardColor;
+  Color femaleCardColor = inActiveCardColor;
+
+  // 1 = male, 2 = female
+  //Gender selectedGender;
+
+  void updateColour(Gender selectedGender) {
+    if (selectedGender == Gender.male) {
+      if (maleCardColor == inActiveCardColor) {
+        maleCardColor = activeCardColor;
+        femaleCardColor = inActiveCardColor;
+      } else {
+        maleCardColor = inActiveCardColor;
+      }
+    }
+    if (selectedGender == Gender.female) {
+      if (femaleCardColor == inActiveCardColor) {
+        femaleCardColor = activeCardColor;
+        maleCardColor = inActiveCardColor;
+      } else {
+        femaleCardColor = inActiveCardColor;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            //ini widget untuk jenis kelamin
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(Gender.male);
+                        //selectedGender = Gender.male;
+                      });
+                    },
+                    child: ReusableCard(
+                      // colour: selectedGender == Gender.male
+                      //     ? maleCardColor
+                      //     : inActiveCardColor,
+                      colour: maleCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(Gender.female);
+                        //selectedGender = Gender.female;
+                      });
+                    },
+                    child: ReusableCard(
+                      // colour: selectedGender == Gender.female
+                      //     ? femaleCardColor
+                      //     : inActiveCardColor,
+                      colour: femaleCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                //ini widget untuk tinggi
+                Expanded(
+                  child: ReusableCard(
+                    colour: inActiveCardColor,
+                  ),
+                ),
+              ],
+            )),
+            Expanded(
+                child: Row(
+              children: [
+                //ini widget untuk mengatur berat dan umur
+                Expanded(
+                  child: ReusableCard(
+                    colour: inActiveCardColor,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: inActiveCardColor,
+                  ),
+                ),
+              ],
+            )),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        ));
+  }
+}

058e20a1715d1b5c6b02221459f28b3444bcfc7f
MuhammadFajarRenaldy <you@example.com>
2021-11-21 11:06:43 +0800

    commit keempat

--- lib/components/icon_content.dart
+++ lib/components/icon_content.dart
@@ -1,11 +1,11 @@
+import 'package:bmi_calculator_praktikum_k/constant.dart';
 import 'package:flutter/material.dart';
 
 class IconContent extends StatelessWidget {
   const IconContent({
-    Key? key,
-    required this.icon,
-    required this.label,
-  }) : super(key: key);
+    @required this.icon,
+    @required this.label,
+  });
 
   final IconData icon;
   final String label;
@@ -17,15 +17,15 @@ class IconContent extends StatelessWidget {
       children: [
         Icon(
           icon,
-          size: 70.0,
+          size: 80.0,
         ),
         SizedBox(
           height: 15.0,
         ),
         Text(
-          label,
-          style: TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98)),
-        )
+          '$label',
+          style: kLabelStyle,
+        ),
       ],
     );
   }
